b831ab4ea5795bb0be7f789e244e05daa035c125
AdamOrmondroyd <Adam.Ormondroyd@gmail.com>
2024-07-10 14:45:32 +0100

    remove unused adaptive_prior() def

--- cobaya/samplers/polychord/polychord.py
+++ cobaya/samplers/polychord/polychord.py
@@ -324,27 +324,6 @@ class polychord(Sampler):
 
             return theta
 
-
-        def adaptive_prior(cube):
-            theta = np.empty_like(cube)
-            ordered_cube = np.array(cube)[self.ordering]
-            for i, name in enumerate(list(self.model.parameterization.sampled_params())):
-                theta[i] = self.model.prior.pdf[i].ppf(ordered_cube[i])
-                if "N" == name:
-                    N_prior = int(theta[i])
-                    N_idx = i
-                    if N_prior > 2:
-                        ordered_cube[i + 1 : i + N_prior - 1] = self.forced_identifiability_transform(ordered_cube[i + 1 : i + N_prior - 1]) # note: -1 is really i+1 and N-2 added together 
-            # print(self._ordered_blocks_flat)
-            # print(theta)
-            # print(f"N = {N_prior}")
-            comm = MPI.COMM_WORLD
-            rank = comm.Get_rank()
-            if N_prior > 2:
-                if not np.all(np.diff(theta[N_idx+1:N_idx + N_prior -1]) > 0):
-                    raise LoggedError(self.log, "not 61016 :(")
-            # print(f"[{rank}] 61016", flush=True)
-
             param_dict = {list(self.model.parameterization.sampled_params())[j]: theta[j] for j in range(len(theta))}
             with open(f"/home/ano23/rds/hpc-work/external_wa/mpi_files/{rank}.txt", "a+") as f:
                 f.write(str(param_dict))
